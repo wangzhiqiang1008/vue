73a34d7eb2783353d98396e325ddcac9edee9f35
louiswangzhiqiang <louiswangzhiqiang@didiglobal.com>
2021-02-18 22:29:51 +0800

    vue

--- vue.config.js
+++ vue.config.js
@@ -43,7 +43,7 @@ module.exports = {
     //添加proxyTable
     proxy: {
       '/vue-admin-template': {
-        target: "http://:8005", // 要访问的接口域名
+        target: "http://111:8005", // 要访问的接口域名
         // ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
 
