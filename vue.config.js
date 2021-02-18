541c6433ae95f20b55ea9046f9f86df7fdd4f61f
louiswangzhiqiang <louiswangzhiqiang@didiglobal.com>
2021-02-18 22:21:01 +0800

    vue

--- vue.config.js
+++ vue.config.js
@@ -43,19 +43,19 @@ module.exports = {
     //添加proxyTable
     proxy: {
       '/vue-admin-template': {
-        target: "http://10.190.7.95:8005", // 要访问的接口域名
+        target: "http://:8005", // 要访问的接口域名
         // ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
 
       },
       '/edu': {
-        target: "http://10.190.7.95:8004", // 要访问的接口域名
+        target: "http://:8004", // 要访问的接口域名
         // ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
 
       },
       '/anymock': {
-        target: "http://10.190.7.95:8005", // anyMock服务
+        target: "http://:8005", // anyMock服务
         //target: "http://localhost:8005", // anyMock服务
         // ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
